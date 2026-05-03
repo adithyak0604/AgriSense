f40d8e09af861c57eb55dfeb98f5fa6516b83691
Adithya Krishna T K <adhi11062004@gmail.com>
2026-05-03 19:44:12 +0530

    Update model_dwnld.py

--- Rec_Models/model_dwnld.py
+++ Rec_Models/model_dwnld.py
@@ -3,7 +3,7 @@ from huggingface_hub import snapshot_download
 # Download only the contents of the 'Banana' folder
 local_snapshot_path = snapshot_download(
     repo_id="Adithyak1106/Crop-Recommendation-Model",
-    # This targets all files specifically within the Banana/ directory
+    # This targets all files specifically within the Crop-Recommendation-Model Repo
     allow_patterns=["/*"], 
     local_dir="./Rec_Models",
     local_dir_use_symlinks=False 
